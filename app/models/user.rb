0d8182ff876496c8f583b08ac13da037dac058c4
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-21 15:32:01 -0400

    added bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  has_secure_password
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
