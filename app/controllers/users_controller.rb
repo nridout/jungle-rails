febb0984f793cd74156f1bce12a6cb9610cf40f0
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-21 17:03:46 -0400

    Added error handling for forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
