ff1b09be0110ec7ce9e21b4bcbda6a615eee155e
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-22 13:59:43 -0400

    Added http basic authenticate for admin pages

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,7 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+                              password: ENV["HTTP_BASIC_PASSWORD"],
+                              if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
 
   def index
     @categories = Category.order(id: :desc).all
