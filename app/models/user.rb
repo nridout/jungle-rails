f4b85a5afc7938167f43b885dd31736f8ed92d3b
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-22 20:12:03 -0400

    Added Reviews & review relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ActiveRecord::Base
+
   before_save { email.downcase! }
+
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,  presence: true, length: { maximum: 50 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
+
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  has_many :reviews
+  has_many :products, through: :reviews
+
 end
