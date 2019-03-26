77a0d09db34c032d236a7c6e0d9fbd762c3ede47
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-26 12:59:10 -0400

    refactored products spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  subject { described_class.new }
+  let(:category) { Category.new(:name => "Furniture") }
+
+  subject {
+    described_class.new(name: "Product", price: 75.00,
+                      quantity: 4, category: category)
+  }
 
   describe 'Associations' do
     it "should belong to a category" do
@@ -12,42 +17,30 @@ RSpec.describe Product, type: :model do
 
   describe 'Validations' do
     it "is valid with valid attributes" do
-      subject.name = "Name"
-      subject.price = 75.00
-      subject.quantity = 3
-      subject.category = Category.new
       expect(subject).to be_valid
     end
 
     it "is not valid without a name" do
-      subject.price = 75.00
-      subject.quantity = 3
-      subject.category = Category.new
+      subject.name = nil
       expect(subject).to_not be_valid
       expect(subject.errors.messages).to include(:name=>["can't be blank"])
     end
 
     it "is not valid without a price" do
-      subject.name = "Name"
-      subject.quantity = 3
-      subject.category = Category.new
+      subject.price_cents = nil
       expect(subject).to_not be_valid
       expect(subject.errors.messages).to include(:price => ["is not a number", "can't be blank"])
       expect(subject.errors.messages).to include(:price_cents => ["is not a number"])
     end
 
     it "is not valid without a quantity" do
-      subject.name = "Name"
-      subject.price = 75.00
-      subject.category = Category.new
+      subject.quantity = nil
       expect(subject).to_not be_valid
       expect(subject.errors.messages).to include(:quantity=>["can't be blank"])
     end
 
     it "is not valid without a category" do
-      subject.name = "Name"
-      subject.price = 75.00
-      subject.quantity = 3
+      subject.category = nil
       expect(subject).to_not be_valid
       expect(subject.errors.messages).to include(:category=>["can't be blank"])
     end
