804ce3916f2cba3cb0f492d5f8f05b5ce11ad224
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-26 00:06:58 -0400

    refactored password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,7 @@ class User < ActiveRecord::Base
                       uniqueness: { case_sensitive: false }
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
-
+  validates :password, length: { minimum: 6 }
   has_many :reviews
 
 end
