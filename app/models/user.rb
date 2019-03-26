4f498d78faf2995a747c4515c11a8d549a0f0fc2
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-26 12:58:14 -0400

    updated email and password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,  presence: true, length: { maximum: 50 },
-                      format: { with: VALID_EMAIL_REGEX },
+                      format: { with: /@/ },
                       uniqueness: { case_sensitive: false }
 
   has_secure_password
   validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
   has_many :reviews
 
 end
